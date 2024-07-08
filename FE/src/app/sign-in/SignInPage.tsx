64148cde87021d5ea1f09af193a84c1713b3fa72
AVPK <awaisahmedshahid@gmail.com>
2024-07-08 18:27:27 +0500

    Login Page, login api, update auth context routes

--- FE/src/app/sign-in/SignInPage.tsx
+++ FE/src/app/sign-in/SignInPage.tsx
@@ -1,7 +1,100 @@
-import React from 'react';
+'use client';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { LoaderCircle } from 'lucide-react';
+import { loginFormSchema, loginFormType } from './types';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { API_ROUTES, PAGE_ROUTES } from '@/constants/API_ROUTES';
+import { useState } from 'react';
+import Link from 'next/link';
+import { apiRouter } from '@/utils/api-router';
+import { useAuthContext } from '@/components/context/AuthContext';
+import { useRouter } from 'next/navigation';
 
-const SignInPage = () => {
-	return <div>SignInPage</div>;
+const defaultValues: loginFormType = {
+  email: '',
+  password: '',
 };
+export default function SignInPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const { login } = useAuthContext();
+  const router = useRouter()
 
-export default SignInPage;
+  const methods = useForm<loginFormType>({
+    defaultValues,
+    resolver: zodResolver(loginFormSchema),
+    delayError: 500,
+  });
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = methods;
+
+  const onSubmit = (values: loginFormType) => {
+    setIsLoading(true);
+    login(values).then((res) => {
+      setIsLoading(false);
+      if (res) {
+        
+      } else {
+        setIsError(true);
+      }
+      router.replace(PAGE_ROUTES.TODOS_LIST);
+    });
+  };
+
+  return (
+    <section className="flex flex-col justify-center items-center h-screen gap-8">
+      <h1 className="text-3xl font-extrabold"></h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle className="text-2xl">Login</CardTitle>
+            <CardDescription>
+              Enter your email below to login to your account. <br />
+              {isError && <span className="text-red-500 text-sm">{'Incorrect email or password'}</span>}
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input {...register('email')} id="email" type="email" placeholder="m@example.com" />
+              {errors.email && <span className="text-red-500 text-sm">{errors.email.message}</span>}
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input {...register('password')} id="password" type="password" />
+              {errors.password && <span className="text-red-500 text-sm">{errors.password.message}</span>}
+            </div>
+          </CardContent>
+          <CardFooter>
+            <div className="w-full">
+              <Button
+                type="submit"
+                className="w-full"
+                disabled={isLoading}
+              >
+                {isLoading && <LoaderCircle className="animate-spin" />}
+
+                <span className="ml-2">Sign in</span>
+              </Button>
+
+              <div className="mt-4 text-center text-sm">
+                Don{`'`}t have an account?{' '}
+                <Link href={PAGE_ROUTES.SIGN_UP} className="underline">
+                  Sign up
+                </Link>
+              </div>
+            </div>
+          </CardFooter>
+        </Card>
+      </form>
+    </section>
+  );
+}
--- FE/src/app/sign-in/page.ts
+++ FE/src/app/sign-in/page.ts
@@ -1,3 +1,4 @@
+'use client';
 import SignInPage from './SignInPage';
 
 export default SignInPage;
