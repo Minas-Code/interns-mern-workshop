6ad5e048359f71787ae8acce7afdb797d40c27c9
AVPK <awaisahmedshahid@gmail.com>
2024-07-08 19:13:42 +0500

    Signup Page API and types

--- FE/src/app/sign-in/types.ts
+++ FE/src/app/sign-in/types.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const loginFormSchema = z.object({
   email: z.string().min(1, 'Email is required').email('Please enter a valid email'),
-  password: z.string().min(1, 'Password is required'),
+  password: z.string().min(1, 'Password is required').min(7, 'Too short'),
 });
 
 export type loginFormType = z.infer<typeof loginFormSchema>;
