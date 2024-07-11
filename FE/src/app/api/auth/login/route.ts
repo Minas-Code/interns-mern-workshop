59ae23a239b562ddc77fac1d3b3eb956faaede27
Moazzam Hameed Paracha <moazzam.hameed@minascode.com>
2024-07-11 16:15:58 +0500

    improve: add security options for cookie set in login api

--- FE/src/app/api/auth/login/route.ts
+++ FE/src/app/api/auth/login/route.ts
@@ -5,7 +5,19 @@ import { NextResponse } from 'next/server';
 export const POST = () => {
   const cookieStore = cookies();
 
-  cookieStore.set('isLoggedIn', 'true');
+  cookieStore.set('isLoggedIn', 'true', {
+    // set path to root, so the cookie is valid for all PAGE_ROUTES
+    path: '/',
+    // set the cookie to expire in 1 week
+    maxAge: 60 * 60 * 24 * 7,
+    // don't allow the cookie to be read from JavaScript
+    secure: true,
+    // only allow cookies to be transmitted over HTTPS
+    httpOnly: true,
+  });
 
-  return NextResponse.json<GlobalApiResponse>({ success: true, data: { message: 'Sign In Successful', result: undefined } });
+  return NextResponse.json<GlobalApiResponse>({
+    success: true,
+    data: { message: 'Sign In Successful', result: undefined },
+  });
 };
