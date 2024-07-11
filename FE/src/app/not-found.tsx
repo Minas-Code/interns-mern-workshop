38705dd5fbddc70690b1d84aa9b0f69b9dd0c7d5
Moazzam Hameed Paracha <moazzam.hameed@minascode.com>
2024-07-11 16:18:49 +0500

    improve: added return to last page button in 404 page

--- FE/src/app/not-found.tsx
+++ FE/src/app/not-found.tsx
@@ -1,7 +1,13 @@
-import { Button } from "@/components/ui/button";
-import { PAGE_ROUTES } from "@/constants/API_ROUTES";
+import { Button } from '@/components/ui/button';
+import { PAGE_ROUTES } from '@/constants/API_ROUTES';
+import { cookies } from 'next/headers';
 import Link from 'next/link';
+
 export default function Custom404() {
+  const cookieStore = cookies();
+
+  const reqOrigin = cookieStore.get('reqOrigin')?.value || PAGE_ROUTES.BASE;
+
   return (
     <section className="flex flex-col justify-center items-center h-screen gap-4">
       <div>
@@ -11,9 +17,9 @@ export default function Custom404() {
         <h2 className="text-2xl">Page not found</h2>
       </div>
       <div>
-        <Link href={PAGE_ROUTES.BASE} className="underline">
+        <Link href={reqOrigin} className="underline">
           <Button>
-            <span className="text-center">Home</span>
+            <span className="text-center">Return to last Page</span>
           </Button>
         </Link>
       </div>
